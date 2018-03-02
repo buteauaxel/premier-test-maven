ed559c903709cb5236c6fa4b2887328a376dd888
Axel Buteau <buteauaxel@gmail.com>
2018-03-02 11:45:09 +0100

    Affichage avec jfiglet

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -8,7 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
+        String asciiArt = FigletFont.convertOneLine("App");
+        System.out.println(asciiArt);
 
     }
 }
